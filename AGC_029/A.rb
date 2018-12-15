7550c640da219db331981b64a1cfc74bace71fc8
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-12-15 22:55:29 +0900

    AGC_029

---
+++ AGC_029/A.rb
@@ -0,0 +1,13 @@
+a = gets.chomp
+count = 0
+
+(a.length*5/9).times do
+  (0..(a.length)).reverse_each do |i|
+    if a[i] == "B" && a[i+1] == "W"
+      a[i], a[i+1] = "W", "B"
+      count += 1
+    end
+  end
+end
+
+puts count
