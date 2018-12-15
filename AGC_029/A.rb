a4cc1ce4aa590967dfac949921129a709062f60d
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-12-15 22:58:46 +0900

    AGC_029

--- AGC_029/A.rb
+++ AGC_029/A.rb
@@ -1,16 +1,16 @@
-def BW_reverse(a, length, count)
-  (0..(length-@count/2)).reverse_each do |i|
+def BW_reverse(a, count)
+  (0..(LENGTH-count/2)).reverse_each do |i|
     if a[i] == "B" && a[i+1] == "W"
       a[i], a[i+1] = "W", "B"
       count += 1
-      return BW_reverse(a, length, count)
+      return BW_reverse(a, count)
     end
   end
   return count
 end
 
 a = gets.chomp.split("")
-length = a.length - 2
+LENGTH = a.length - 2
 count = 0
 
-puts BW_reverse(a, length, count)
+puts BW_reverse(a, count)
